c479d3e43471cee15905c0f77868f279069647f3
Yannik Schmidt <yannik.schmidt@thermoscan.de>
2021-03-21 16:41:21 +0100

    implement basic http to influx

--- main/main.ino
+++ main/main.ino
@@ -5,7 +5,8 @@
 #include <Adafruit_SSD1306.h>
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
-
+#include <ESP8266HTTPClient.h>
+#include <base64.h>
 #define ONE_SECOND 1000
 #define TEXT_SIZE  2
 
@@ -13,8 +14,8 @@
 
     /* OLED_RESET-pin to '-1' because reset pin is shared */
     #define OLED_RESET    -1
-    #define SCREEN_WIDTH 128 
-    #define SCREEN_HEIGHT 32 
+    #define SCREEN_WIDTH 128
+    #define SCREEN_HEIGHT 32
 
     Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
@@ -29,38 +30,40 @@ SimpleDHT11 dht11(pinDHT11);
 #include "settings.h"
 
 /* clients */
-WiFiClient espHome;
+WiFiClientSecure client;
 
 void setup() {
 
   Serial.begin(SERIAL_DEFAULT);
 
-  /* SSD1306_SWITCHCAPVCC = generate OLED-display voltage from 3.3V internally */
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { /* Address 0x3C for 128x32 */
-    Serial.println(F("SSD1306 allocation failed"));
-    for(;;);
-  }
-
-  //check Wifi:
+  /* check Wifi */
   WiFi.begin(WLAN_SSID, WLAN_PASSWORD);
   while (WiFi.status() != WL_CONNECTED){
     delay(ONE_SECOND);
   }
 
   #ifdef with_display
+
+    /* SSD1306_SWITCHCAPVCC = generate OLED-display voltage from 3.3V internally */
+    if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { /* Address 0x3C for 128x32 */
+      Serial.println(F("SSD1306 allocation failed"));
+      for(;;);
+    }
+
     display.clearDisplay();
     display.setTextSize(TEXT_SIZE);
     display.setTextColor(SSD1306_WHITE);
     display.setCursor(0, 0);
     display.cp437(true);
     display.display();
+
   #endif
-  
+
 }
 
 void loop() {
- 
-  /* read sensor */ 
+
+  /* read sensor */
   byte temperature = 0;
   byte humidity    = 0;
   int  err         = SimpleDHTErrSuccess;
@@ -83,13 +86,19 @@ void loop() {
     display.display();
   #endif
 
-  String tempText = "temp,location=" + SENSOR_LOCATION + " value=" + String(temperature);
-  String humText  = "hum,location="  + SENSOR_LOCATION + " value=" + String(humidity);
-
   client.setInsecure();
   client.connect(TARGET_INFLUX_SERVER, TARGET_INFLUX_PORT);
-  http.begin(client, host);
-  auto ret = http.POST()
-  
+  HTTPClient http;
+  http.begin(client, TARGET_INFLUX_SERVER);
+
+  String dataTemp = "temperatur,location=" + String(SENSOR_LOCATION) +",value=" + String(temperature);
+  String dataHum  = "humidity,location="   + String(SENSOR_LOCATION) +",value=" + String(humidity);
+
+  String auth = base64::encode(String(INFLUX_AUTH_USER) + ":" + String(INFLUX_AUTH_PW));
+  http.addHeader("Authorization", "Basic " + auth);
+
+  auto retTemp = http.POST(dataTemp);
+  auto retHum  = http.POST(dataHum);
+
   delay(MESSURMENT_DELAY);
 }
